78f943f215d7f1994ff9ab83b68e61cb5dd31c60
khanh <khanh.nguyen.contact@gmail.com>
2025-03-18 21:17:48 +0800

    auto_push

--- main.cpp
+++ main.cpp
@@ -140,7 +140,7 @@ struct interpreter {
     }
 };
 
-vec<char> read_file(const char* filename) {
+vec<char> read_code_from_file(const char* filename) {
     FILE* code_file = std::fopen(filename, "r");
     if (code_file == nullptr) {
         std::printf("Error: cannot open file %s\n", filename);
@@ -155,16 +155,22 @@ vec<char> read_file(const char* filename) {
     return code;
 }
 
+std::tuple<uint64, vec<char>> read_args(int argc, char** argv) {
+    if (argc < 2) {
+        std::printf("Usage: %s <data_length> <code_filename>\n", argv[0]);
+        return {0, vec<char>()};
+    }
+    uint64 data_length = std::stoull(argv[1]);
+    vec<char> code = read_code_from_file(argv[2]);
+    return {data_length, code};
+}
+
 int main(int argc, char** argv) {
     if (argc < 2) {
         std::printf("Usage: %s <data_length> <code_filename>\n", argv[0]);
         return 1;
     }
-
-    uint64 data_length = std::stoull(argv[1]);
-    char* code = read_file(argv[2]);
-
-
+    auto [data_length, code] = read_args(argc, argv);
 
     interpreter i(data_length, code);
     while (i.step()) {
