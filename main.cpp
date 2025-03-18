2e6e1be7c74be1fba269b04a4d5bc14cf22b33ce
khanh <khanh.nguyen.contact@gmail.com>
2025-03-18 20:14:41 +0800

    auto_push

--- main.cpp
+++ main.cpp
@@ -9,35 +9,77 @@ using vec = std::vector<T>;
 template<typename KT, typename VT>
 using dict = std::unordered_map<KT, VT>;
 
-struct prog {
+struct interpreter {
+    uint64 data_ptr;
+    uint64 code_ptr;
+    vec<char> data;
+    interpreter(uint64 count): data_ptr(0), code_ptr(0), data(count, 0) {}
+
     const char* code;
     dict<uint64, uint64> jump_table;
-    prog(const char* code) : code(code) {
+    void load_code(const char* code, bool debug = true) {
         vec<uint64> bracket_index_stack;
         for (uint64 i=0; i<std::strlen(code); i++) {
             switch (code[i]) {
                 case '[':
                     bracket_index_stack.push_back(i);
-                    continue;
+                    break;
                 case ']':
                     uint64 j = bracket_index_stack.back();
                     bracket_index_stack.pop_back();
                     jump_table[i] = j;
                     jump_table[j] = i;
-                    continue;
+                    break;
+            }
+        }
+        if (debug) {
+            std::printf("jump table:\n");
+            for (auto& [k, v] : jump_table) {
+                if (k < v) {
+                    std::printf("\t%llu <-> %llu\n", k, v);
+                }
             }
         }
     }
-    void print_jump_table() {
-        for (auto& [k, v] : jump_table) {
-            std::printf("%llu -> %llu\n", k, v);
+
+    void step() {
+        switch (code[code_ptr]) {
+            case '>':
+                data_ptr++;
+                break;
+            case '<':
+                data_ptr--;
+                break;
+            case '+':
+                data[data_ptr]++;
+                break;
+            case '-':
+                data[data_ptr]--;
+                break;
+            case '.':
+                std::putchar(data[data_ptr]);
+                break;
+            case ',':
+                data[data_ptr] = std::getchar();
+                break;
+            case '[':
+                if (data[data_ptr] == 0) {
+                    code_ptr = jump_table[code_ptr];
+                }
+                break;
+            case ']':
+                if (data[data_ptr] != 0) {
+                    code_ptr = jump_table[code_ptr];
+                }
+                break;
         }
+        code_ptr++;        
     }
 };
 
 
 int main() {
-    prog p("[caca][cac[ccc]]");
-    p.print_jump_table();
+    interpreter i(30000);
+    i.load_code("[caca][cac[ccc]]");
     return 0;
 }
