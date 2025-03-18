ede716acddd8ea3c27d62ffc5ef8a4041d89297b
khanh <khanh.nguyen.contact@gmail.com>
2025-03-19 04:50:01 +0800

    auto_push

--- cmd/universal_search/main.go
+++ cmd/universal_search/main.go
@@ -3,12 +3,29 @@ package main
 import (
 	"brainfuck_go/pkg/universal_search"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
-	input := []uint8("6")
+	if len(os.Args) < 2 {
+		panic("Usage: universal_search_factorization <num>")
+	}
+
+	num, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	input := []uint8(strconv.Itoa(num))
 	test := func(s []uint8) bool {
-		return "2" == string(s)
+		factor, err := strconv.Atoi(string(s))
+		if err != nil {
+			return false
+		}
+		if factor < 2 || factor >= num {
+			return false
+		}
+		return num%factor == 0
 	}
 
 	output, code := universal_search.UniversalSearch(64, input, test)
